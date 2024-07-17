9f996cc64eb42d9eedc734d145b5b35ed317b695
yagnik1125 <yagnikvasoya1125@gmail.com>
2024-07-17 15:54:39 +0530

    17/07/2024-15:54

--- lib/userpages/groupexpensepage.dart
+++ lib/userpages/groupexpensepage.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-class GroupController extends StatelessWidget {
-  const GroupController({super.key});
+class GroupExpensePage extends StatelessWidget {
+  const GroupExpensePage({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold();
